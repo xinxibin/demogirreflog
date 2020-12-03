2e547bb45c1c5a602cde8161c05c26d0c511f6d7
Fox <506907778@qq.com>
2020-12-03 11:32:02 +0800

    secondCommit

---
+++ app.config.js
@@ -0,0 +1,124 @@
+export default {
+
+	pages: [
+        // 用户体验列表
+        'pages/usertiyan/index',
+        // 用户体验详情
+        'pages/usertiyan/usertiyandetail/index',
+        // 课堂详情
+        'pages/classroomdetail/index',
+        // 网络课堂
+        'pages/networkclassroom/index',
+        // 直播列表
+        'pages/livelist/index',
+        // 公告详情
+        'pages/gonggaodetail/index',
+        // 会员专享
+        'pages/vipzhuanxiang/index',
+		// 积分商品
+        'pages/myintegral/gooosdetail/index',
+        // 积分订单详情
+        'pages/myintegral/integralmallorder/integralorderDetail/index',
+        // 积分兑换订单
+        'pages/myintegral/integralmallorder/index',
+        // 兑换成功
+        'pages/myintegral/integraldetail/exchange/index',
+		// 积分商城订单
+		'pages/myintegral/integraldetail/order/index',
+		// 积分商城
+		'pages/myintegral/integraldetail/integralmall/index',
+		// 积分规则
+		'pages/myintegral/integraldetail/index',
+		// 我的积分
+		'pages/myintegral/index',
+		// 店铺详情
+		'pages/shopdetail/index',
+		'pages/index/index',
+		//店铺优惠劵
+		'pages/coupon/coupon',
+		// 登录
+		'pages/login/login',
+		// 分类
+		'pages/classifica/classifica',
+		// 服务预约
+		'pages/serviceBook/serviceBook',
+		// 预约报名
+		'pages/appointment/appointment',
+		// 商品详情
+		'pages/productDetail/productDetail',
+		// 订单结算
+		'pages/submitOrder/submitOrder',
+		// 限时抢购
+		'pages/rushPurchase/rushPurchase',
+		// 团购
+		'pages/groupBuy/groupBuy',
+		// 限量爆款
+		'pages/limitedSales/limitedSales',
+		// 助力
+		'pages/help/help',
+		// 我的
+		'pages/center/center',
+		// 个人资料
+		'pages/personInfo/personInfo',
+		// 提现
+		'pages/withdrawal/withdrawal',
+		// 余额明细
+		'pages/balanceDetails/balanceDetails',
+		// 服务详情
+		'pages/serviceDetail/serviceDetail',
+		// 提现详情
+		'pages/withDetail/withDetail',
+		// 我的订单
+		'pages/orderList/orderList',
+		// 订单详情
+		'pages/orderDetail/orderDetail',
+		// 选择优惠券
+		'pages/selectCoupon/selectCoupon',
+		// 发布评价
+		'pages/evaluate/evaluate',
+		// 评价详情
+		'pages/evaluateDetail/evaluateDetail',
+		// 浏览足迹
+		'pages/browseFoot/browseFoot',
+		// 我的优惠券
+		'pages/myCoupon/myCoupon',
+		// 历史记录
+		'pages/historyRecord/historyRecord',
+		// 优惠券详情
+		'pages/couponDetail/couponDetail',
+		// 消息中心
+		'pages/messageCenter/messageCenter',
+		// 消息详情
+		'pages/messageDetail/messageDetail',
+		// 编辑地址
+		'pages/editAddress/editAddress',
+		// 地址管理
+		'pages/addressList/addressList',
+		// 任务中心
+		'pages/taskCenter/taskCenter',
+		// 签到
+		'pages/signIn/signIn',
+		// 分销中心
+		'pages/disCenter/disCenter',
+		// 分销
+		'pages/distribution/distribution',
+		// 推广客户
+		'pages/extension/extension',
+		// 佣金记录
+		'pages/commRecord/commRecord',
+		// 达人排行榜
+		'pages/talentRank/talentRank',
+		// 系统设置
+		'pages/sysSet/sysSet',
+		// 意见反馈
+		'pages/feedback/feedback',
+		// 关于我们
+		'pages/aboutUs/aboutUs',
+	],
+	window: {
+		backgroundTextStyle: 'light',
+		navigationBarBackgroundColor: '#F30841',
+		navigationBarTitleText: 'WeChat',
+		navigationBarTextStyle: 'white'
+	}
+}
